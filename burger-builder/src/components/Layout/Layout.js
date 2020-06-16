8bd63d4262463a5caa06357b7387cf6c13252805
Nikola Novak <nikolan97@live.com>
2020-06-17 00:19:39 +0200

    Added a backdrop component

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -3,11 +3,11 @@ import React from "react";
 import Aux from "../../hoc/Auxiliary";
 import styles from "./Layout.module.css";
 
-const Layout = (props) => (
+const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
 
-export default Layout;
+export default layout;
