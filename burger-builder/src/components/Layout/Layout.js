1a2a30c1e7ba2e9c44cad4e9b1c2c9115e409e38
Nikola Novak <nikolan97@live.com>
2020-06-17 01:53:46 +0200

    Adjusting the header (toolbar/side drawer)  to be responsive

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import Aux from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import styles from "./Layout.module.css";
 
 const layout = (props) => (
   <Aux>
     <Toolbar />
-    <div>SideDrawer</div>
+    <SideDrawer />
     <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
