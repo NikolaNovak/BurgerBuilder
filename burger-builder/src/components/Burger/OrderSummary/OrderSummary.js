285eda2eccb5b1297c12e5c950a6437ed6cec615
Nikola Novak <nikolan97@live.com>
2020-06-17 00:32:13 +0200

    Added an accept/cancel button

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import Aux from "../../../hoc/Auxiliary";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
       <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
         {props.ingredients[igKey]}
       </li>
     );
@@ -18,6 +19,12 @@ const orderSummary = (props) => {
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <Button buttonType="Danger" clicked={props.purchaaseCancel}>
+        CANCEL
+      </Button>
+      <Button buttonType="Success" clicked={props.purchaseAccept}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
