dd9a824709ae61e93d88b501b5f4fed0b0dc6348
Nikola Novak <nikolan97@live.com>
2020-06-17 19:22:35 +0200

    Added open/close functionality and a backdrop to the side drawer

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,28 @@ import React from "react";
 
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxiliary";
 import styles from "./SideDrawer.module.css";
 
 const sideDrawer = (props) => {
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+  if (props.show) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
+
   return (
-    <div className={styles.SideDrawer}>
-      <div className={styles.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.show} clicked={props.close} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
