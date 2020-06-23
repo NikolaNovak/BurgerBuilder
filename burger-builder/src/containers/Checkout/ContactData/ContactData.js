321a463f974e281ca09c5352c11eae754c4d8e53
Nikola Novak <nikolan97@live.com>
2020-06-24 01:53:09 +0200

    Created a custom dynamic Input component

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import axios from "../../../axios-orders";
 
 import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Input/Input";
 import classes from "./ContactData.module.css";
 
 class ContactData extends Component {
@@ -51,26 +52,21 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="name"
           placeholder="Full Name"
         />
-        <input
-          className={classes.Input}
-          type="text"
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          className={classes.Input}
+        <Input inputType="input" type="text" name="email" placeholder="Email" />
+        <Input
+          inputType="input"
           type="text"
           name="street"
           placeholder="Street"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputType="input"
           type="text"
           name="postal"
           placeholder="Postal Code"
