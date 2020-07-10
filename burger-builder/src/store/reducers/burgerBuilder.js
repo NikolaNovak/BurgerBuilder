72e3f03867a09e20ef7a28c0601f77bfa031b87b
Nikola Novak <nikola.novak.hr@gmail.com>
2020-07-11 00:28:38 +0200

    Sharing the updateObject & checkValidity utility functions

--- burger-builder/src/store/reducers/burgerBuilder.js
+++ burger-builder/src/store/reducers/burgerBuilder.js
@@ -1,6 +1,6 @@
 import * as actionTypes from "../actions/actionTypes";
 import * as constants from "../../constants/ingredient-prices";
-import { updateObject } from "../utility";
+import { updateObject } from "../../shared/utility";
 
 const initialState = {
   ingredients: null,
